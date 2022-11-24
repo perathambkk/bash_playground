a52e935cca5ca09ec2a206996edde9c8a0a8affc
Peratham <75246140+perathambkk@users.noreply.github.com>
2022-11-24 16:54:32 +0700

    Add files via upload

---
+++ leetcode/leet.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+file="file.txt"
+
+while read -r line; do
+    if [[ $line =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]] ||
+    [[ $line =~ ^\([0-9]{3}\)[[:space:]][0-9]{3}-[0-9]{4}$ ]] 
+    then
+        echo -e "$line"
+    fi
+done <$file 
